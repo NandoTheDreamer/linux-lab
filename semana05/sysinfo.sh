73f91c2d926eab4cb7e146c7921b89e7760f323c
Fernando Flores <carlosfernandofloresu@gmail.com>
2026-03-15 14:04:46 -0400

    feat: add CPU and memory sections

--- semana05/sysinfo.sh
+++ semana05/sysinfo.sh
@@ -48,7 +48,7 @@ case "$MODO" in
 	exit 0 ;;
 	--help|-h)		uso ;;
 	*)
-		echo "Error: opcion desconocida ’$MODO’"
+		echo "Error: opcion desconocida '$MODO'"
 		uso
 		;;
 esac
@@ -61,16 +61,16 @@ echo ""
 
 #===Seccion 1: Informacion general===
 seccion_general () {
-        echo "[INFORMACION DEL SISTEMA ]"
-        echo "$SEPARADOR_SEC"
-        printf "        %-18s %s\n" "Hostname:"         "$(hostname)"
-        printf "        %-18s %s\n" "Usuario:"          "$USER"
-        printf "        %-18s %s\n" "Sistema:"          "$(uname -s)"
-        printf "        %-18s %s\n" "Kernel:"           "$(uname -r)"
-        printf "        %-18s %s\n" "Arquitectura:"     "$(uname -m)"
-        printf "        %-18s %s\n" "Fecha/Hora:"       "$(date ’+%d/%m/%Y%H:%M:%S’)"
-        printf "        %-18s %s\n" "Encendido:"        "$(uptime -p)"
-        echo ""
+	echo "[INFORMACION DEL SISTEMA ]"
+	echo "$SEPARADOR_SEC"
+	printf "	%-18s %s\n" "Hostname:"		"$(hostname)"
+	printf "	%-18s %s\n" "Usuario:"		"$USER"
+	printf "	%-18s %s\n" "Sistema:"		"$(uname -s)"
+	printf "	%-18s %s\n" "Kernel:"		"$(uname -r)"
+	printf "	%-18s %s\n" "Arquitectura:"	"$(uname -m)"
+	printf "	%-18s %s\n" "Fecha/Hora:"	"$(date '+%d/%m/%Y%H:%M:%S')"
+	printf "	%-18s %s\n" "Encendido:"	"$(uptime -p)"
+	echo ""
 }
 
 #===Seccion 2: CPU===
@@ -78,7 +78,7 @@ seccion_cpu() {
 	echo "[ CPU ]"
 	echo "$SEPARADOR_SEC"
 	nucleos=$(nproc)
-	carga=$(uptime | awk -F’loadaverage:’ ’{print $2}’ \ | awk ’{print $1}’ | tr -d ’,’)
+	carga=$(uptime | awk -F'loadaverage:' '{print $2}' \ | awk '{print $1}' | tr -d ',')
 	printf " %-18s %s\n" "Nucleos:"			"$nucleos"
 	printf " %-18s %s\n" "Carga (1min):"	"$carga"
 	echo ""
@@ -88,11 +88,11 @@ seccion_cpu() {
 seccion_memoria() {
 	echo "[ MEMORIA RAM ]"
 	echo "$SEPARADOR_SEC"
-	mem_total=$(free -h | awk ’/^Mem:/ {print$2}’)
-	mem_usado=$(free -h | awk ’/^Mem:/ {print$3}’)
-	mem_libre=$(free -h | awk ’/^Mem:/ {print$4}’)
-	swap_total=$(free -h | awk ’/^Swap:/ {print$2}’)
-	swap_usado=$(free -h | awk ’/^Swap:/ {print$3}’)
+	mem_total=$(free -h | awk '/^Mem:/ {print$2}')
+	mem_usado=$(free -h | awk '/^Mem:/ {print$3}')
+	mem_libre=$(free -h | awk '/^Mem:/ {print$4}')
+	swap_total=$(free -h | awk '/^Swap:/ {print$2}')
+	swap_usado=$(free -h | awk '/^Swap:/ {print$3}')
 	printf " %-18s %s\n" "RAM total:"	"$mem_total"
 	printf " %-18s %s\n" "RAM usada:"	"$mem_usado"
 	printf " %-18s %s\n" "RAM libre:"	"$mem_libre"
