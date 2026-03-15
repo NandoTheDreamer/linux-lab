ea9707603a19972092a7a65e6dd467c57b309ff7
Fernando Flores <carlosfernandofloresu@gmail.com>
2026-03-15 14:13:58 -0400

    feat: add disk usage section

--- semana05/sysinfo.sh
+++ semana05/sysinfo.sh
@@ -54,7 +54,7 @@ case "$MODO" in
 esac
 
 echo "$SEPARADOR"
-printf "  REPORTEDELSISTEMA - sysinfo.sh v%s\n" "
+printf "  REPORTE DEL SISTEMA - sysinfo.sh v%s\n" "
 	$VERSION"
 echo "$SEPARADOR"
 echo ""
@@ -101,13 +101,25 @@ seccion_memoria() {
 	echo ""
 }
 
+#===Seccion 4: Disco===
+seccion_disco() {
+	echo "[ USO DE DISCO ]"
+	echo "$SEPARADOR_SEC"
+	printf " %-20s %6s %6s %6s %5s\n" \ "Particion" "Total" "Usado" "Libre" "Uso%"
+	echo " $(printf '%.0s-' {1..48})"
+	df -h | grep -v "^tmpfs\|^udev\|^Filesystem" | \ awk '{printf "  %-20s %6s %6s %6s %5s\n", $6, $2, $3, $4, $5}'
+	echo ""
+}
+
 #===Ejecutar segun el modo===
 case "$MODO" in
 	all)
 		seccion_general
 		seccion_cpu
 		seccion_memoria
+		seccion_disco
 		;;
-	cpu ) seccion_cpu ;;
-	mem ) seccion_memoria ;;
+	disk) seccion_disco ;;
+	cpu) seccion_cpu ;;
+	mem) seccion_memoria ;;
 esac
